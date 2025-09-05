04497f4bd98cfb83f6df63469fa4d816009ab94e
hassi999 <huzaifak076@gmail.com>
2025-09-05 08:59:16 +0500

    added project files

---
+++ src/components/ResultModal.jsx
@@ -0,0 +1,27 @@
+import { createPortal } from "react-dom";
+
+export default function ResultModal({ targetTime, remainingTime, onReset, ref }) {
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
+
+  return createPortal(
+    <dialog ref={ref} className="result-modal" onClose={onReset}>
+      {userLost && <h2>You Lost</h2>}
+      {!userLost && <h2>Your score: {score}</h2>}
+      <p>
+        The target time was <strong>{targetTime} second{targetTime > 1 ? "s" : ""}.</strong>
+      </p>
+      <p>
+        You stopped the timer with
+        <strong>{formattedRemainingTime} seconds left.</strong>
+      </p>
+      <form method="dialog" onSubmit={onReset}>
+        <button>Close</button>
+      </form>
+    </dialog>,
+    document.getElementById("modal")
+  );
+}; 
+
+
